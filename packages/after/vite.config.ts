1b8760bd01be3eb9209851c73137a2b392319936
jy0813 <busbug3@gmail.com>
2025-11-28 10:04:04 +0900

    chore: vite.config.ts 설정 추가

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -1,24 +1,30 @@
-import { defineConfig } from 'vite';
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
+import { defineConfig as defineViteConfig, mergeConfig } from 'vite';
+import { defineConfig as defineTestConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
-export default mergeConfig(
-  defineConfig({
+export default defineViteConfig(({ mode }) => {
+  const viteConfig = defineViteConfig({
+    // 여기서 mode 사용 가능
+    base: mode === 'production' ? '/front_7th_chapter3-1/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
       },
     },
-  }),
-  defineTestConfig({
+  });
+
+  const testConfig = defineTestConfig({
     test: {
       globals: true,
       environment: 'jsdom',
       setupFiles: './src/test/setup.ts',
       css: true,
     },
-  }),
-);
+  });
+
+  // 두 설정을 병합하여 반환
+  return mergeConfig(viteConfig, testConfig);
+});
